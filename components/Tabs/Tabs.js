d864992a7866690163cbf43c1266eff5620e2a88
rifqifinaldy <rifqifinaldy@gmail.com>
2022-05-19 22:56:50 +0700

    Add Footer Section

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import Typography from "@mui/material/Typography";
+import { Box } from "@mui/system";
+import PropTypes from "prop-types";
 
-const Tabs = () => {
+export const TabPanel = ({ children, value, index, ...other }) => {
   return (
-    <div>Tabs</div>
-  )
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+};
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+export const a11yProps = (index) => {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
 }
 
-export default Tabs
+
--- pages/index.js
+++ pages/index.js
@@ -2,44 +2,10 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import RFCard from "../components/Cards/Cards";
 import { WEATHER_APP_BASE_URL, WEATHER_APP_ID } from "../utility/global";
-import PropTypes from "prop-types";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
+import { TabPanel, a11yProps } from "../components/Tabs/Tabs";
 
 export default function Home() {
   const [weather, setWeather] = useState({});
@@ -89,39 +55,33 @@ export default function Home() {
       </div>
       {/* Handle error bila Browser Belum Mengizinkan Akses Lokasi */}
       {weather.weather && (
-        <>
-          <div className="card-content">
-            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-              <Tabs
-                value={value}
-                onChange={handleChange}
-                aria-label="basic tabs example"
-              >
-                <Tab label="Item One" {...a11yProps(0)} />
-                <Tab label="Item Two" {...a11yProps(1)} />
-                <Tab label="Item Three" {...a11yProps(2)} />
-              </Tabs>
-            </Box>
-            <TabPanel value={value} index={0}>
-              <RFCard
-                title="Current Weather"
-                subtitle={weather.name}
-                cloudiness={weather.clouds.all}
-                humidity={weather.main.humidity}
-                temperature={weather.main.temp}
-                pressure={weather.main.pressure}
-                icon={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`}
-                description={weather.weather[0].description}
-              />
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-              Item Two
-            </TabPanel>
-            <TabPanel value={value} index={2}>
-              Item Three
-            </TabPanel>
-          </div>
-        </>
+        <div className="card-content">
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              aria-label="basic tabs example"
+            >
+              <Tab label="Current Weather" {...a11yProps(0)} />
+              <Tab label="Forecast" {...a11yProps(1)} />
+            </Tabs>
+          </Box>
+          <TabPanel value={value} index={0}>
+            <RFCard
+              title="Current Weather"
+              subtitle={weather.name}
+              cloudiness={weather.clouds.all}
+              humidity={weather.main.humidity}
+              temperature={weather.main.temp}
+              pressure={weather.main.pressure}
+              icon={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`}
+              description={weather.weather[0].description}
+            />
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            Item Two
+          </TabPanel>
+        </div>
       )}
     </>
   );
